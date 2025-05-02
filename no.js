ed75f4d63f82677e2509bb5ea27210f53743234d
Glibn <romanuting@gmail.com>
2025-05-02 15:43:55 +0300

    я

---
+++ no.js
@@ -0,0 +1,19 @@
+const choose = "Кава";
+switch (choose) {
+  case "Кава":
+    console.log("вибрано каву");
+
+    break;
+
+  case "Чай":
+    console.log("вибрано Чай");
+    break;
+
+  case "Сік":
+    console.log("вибрано Сік");
+    break;
+
+  default:
+    console.log("нічого не вибрано");
+    break;
+}
