95f58f425de811433dac251406ba3fe1b45eaced
Glibn <romanuting@gmail.com>
2025-05-16 23:27:22 +0300

    ч

--- no.js
+++ no.js
@@ -17,3 +17,112 @@
 //     console.log("нічого не вибрано");
 //     break;
 // }
+// const day = "понеділок";
+// switch (day) {
+//   case "понеділок":
+//     console.log("робочий день");
+//     break;
+//   case "Вівторок":
+//     console.log("робочий день");
+//     break;
+//   case "Середа":
+//     console.log("робочий день");
+//     break;
+//   case "Четверг":
+//     console.log("робочий день");
+//     break;
+//   case "П'ятниця":
+//     console.log("робочий день");
+//     break;
+//   case "Субота":
+//     console.log("вихідний день");
+//     break;
+//   case "Неділя":
+//     console.log("вихідний день");
+//     break;
+//   default:
+//     console.log("нічого не вибрано");
+//     break;
+// }
+// const month = "8";
+// switch (month) {
+//   case "1":
+//     console.log("Зима");
+//     break;
+//   case "2":
+//     console.log("Зима");
+//     break;
+//   case "3":
+//     console.log("Зима");
+//     break;
+//   case "4":
+//     console.log("Весна");
+//     break;
+//   case "5":
+//     console.log("Весна");
+//     break;
+//   case "6":
+//     console.log("Весна");
+//     break;
+//   case "7":
+//     console.log("Літо");
+//     break;
+//   case "8":
+//     console.log("Літо");
+//     break;
+//   case "9":
+//     console.log("Літо");
+//     break;
+//   case "10":
+//     console.log("Осінь");
+//     break;
+//   case "11":
+//     console.log("Осінь");
+//     break;
+//   case "12":
+//     console.log("Осінь");
+//     break;
+//   default:
+//     console.log("нічого не вибрано");
+//     break;
+// }
+// const color = "Green";
+// switch (color) {
+//   case "Red":
+//     console.log("стоп");
+//     break;
+//   case "Green":
+//     console.log("йти");
+//     break;
+//   case "Yellow":
+//     console.log("чекати");
+//     break;
+//   default:
+//     console.log("нічого не вибрано");
+//     break;
+// }
+// let num1 = 10;
+// let num2 = 3;
+// let operator = "/";
+// let result;
+// switch (operator) {
+//   case "+":
+//     result = num1 + num2;
+//     break;
+//   case "-":
+//     result = num1 - num2;
+//     break;
+//   case "*":
+//     result = num1 * num2;
+//     break;
+//   case "/":
+//     if (num2 === 0) {
+//       result = "неможна ділити на 0";
+//     } else {
+//       result = num1 / num2;
+//     }
+//     break;
+//   default:
+//     result = "Невідомий оператор";
+// }
+// console.log("Результат:", result);
